f05f577273e81b8e324341e909d7ba71009f612e
Thbop <75861321+thbop@users.noreply.github.com>
2025-08-29 16:38:06 -0400

    Part B, guessing number game

---
+++ app/src/main/java/app/Guess.java
@@ -0,0 +1,40 @@
+package app;
+
+import java.util.*;
+
+public class Guess {
+    public static void main( String[] args ) {
+        var game = new Game();
+        game.run();
+    }
+}
+
+class Game {
+    private int secretNumber;
+    private int guessCount = 0;
+    private int guess = 0;
+
+
+    public Game() {
+        secretNumber = (int)(Math.random() * 10) + 1;
+    }
+
+    void run() {
+        var scan = new Scanner( System.in );
+        while ( guess != secretNumber ) {
+            guess = Integer.parseInt( scan.nextLine() );
+            guessCount++;
+
+            // Feedback
+            if ( guess < secretNumber )
+                System.out.println( "Your guess is too low." );
+            else if ( guess > secretNumber )
+                System.out.println( "Your guess is too high." );
+            else
+                System.out.println( "You got it!!" );
+        }
+        scan.close();
+
+        System.out.println( "It took you " + guessCount + " guesses." );
+    }
+}
